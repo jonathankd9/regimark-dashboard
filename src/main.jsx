0f5dc869b16c0c374a27a62a4e4193269d69a39d
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-07-01 14:23:28 +0000

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "vuesax/dist/vuesax.css";
 import "./index.css";
-import AuthContext from "./context/AuthProvider.jsx";
+import AuthProvider from "./context/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<AuthContext.Provider>
+		<AuthProvider>
 			<App />
-		</AuthContext.Provider>
+		</AuthProvider>
 	</React.StrictMode>
 );
