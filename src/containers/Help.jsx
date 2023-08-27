11f833e67fee06e7e0b4a29defcdefbafacd0b72
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-08-27 19:35:01 +0000

    update

--- src/containers/Help.jsx
+++ src/containers/Help.jsx
@@ -17,8 +17,47 @@ const Help = () => {
 
 				{/* Help information here */}
 
-				<div className="flex flex-grow bg-white rounded-2xl justify-center items-center min-h-screen">
-					<h1 className="text-primary">Help and Information Here!</h1>
+				<div className="flex gap-16 py-5 px-10 bg-white rounded-2xl justify-start h-screen">
+					<div className="w-[50%]">
+						<h1 className="text-primary">How it works?</h1>
+						<p className="mb-10 text-lg">
+							Step-by-step process for instructors
+						</p>
+						<div className="text-xl flex flex-col gap-5">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+								euismod, diam at bibendum consectetur, ligula lorem malesuada
+								ex, nec volutpat neque sapien in dolor.
+							</p>
+							<p>
+								Nunc euismod fermentum orci, in volutpat tortor lacinia a.
+								Aenean vehicula convallis urna, ac malesuada tellus viverra sit
+								amet. Nulla facilisi. Vivamus hendrerit aliquet odio, non
+								finibus ligula tristique vel. Curabitur in urna mi. Nullam eu
+								dui vel ligula efficitur rhoncus.
+							</p>
+							<p>
+								In hac habitasse platea dictumst. Ut ullamcorper urna sit amet
+								ante varius, sit amet efficitur dui eleifend. Sed euismod, diam
+								at bibendum consectetur, ligula lorem malesuada ex, nec volutpat
+								neque sapien in dolor.
+							</p>
+						</div>
+					</div>
+					<div className="w-[50%]">
+						<h1 className="text-primary">FAQs</h1>
+						<p className="mb-10 text-lg">Questions asked often?</p>
+
+						<div className="">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+							</p>
+							<p>Nunc euismod fermentum orci, in volutpat tortor lacinia a.</p>
+							<p>
+								In hac habitasse platea dictumst. Ut ullamcorper urna sit amet
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
